05f24d10c8a0a6fc8cf3b0ab5c427606dee0fb16
vecoweb <vecoweb22@yandex.ru>
2023-06-23 09:58:18 +0300

    Fix constants

--- backend/utils/constants.js
+++ backend/utils/constants.js
@@ -4,7 +4,7 @@ const URL_REG_EXP = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9
 
 const {
   NODE_ENV,
-  JWT_SECRET = 'c7d13632d38f2f55e1dd7cdc5f50e925cd86c2db1b890c2f8561c9012dde29cf',
+  JWT_SECRET,
   PORT = 3000,
   DB_URI = 'mongodb://127.0.0.1:27017/mestodb',
 } = process.env;
